b4571fea61895cc97b72365400a45cbf14eecb2a
Kayhan.Kashi@gmail.com <Kayhan.Kashi@gmail.com>
2020-09-06 21:34:39 +0430

    Lifting state up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Counters from "./components/counters";
 import Counter from "./components/counter";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 0, value: 4 },
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    //console.log("eventHandler called", counterId);
+    const counters = this.state.counters.filter((c) => c.id != counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    // counter.value++;
+    // const counters = this.state.counters;
+    // const index = counters.indexOf(counter);
+    // counters[index] = counter;
+    // this.setState({ counters });
+
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
+            counters={this.state.counters}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
